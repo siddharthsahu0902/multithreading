e8e6787f61f71fb84db171dda496a70649c855b5
siddharthsahu0902 <siddharthsahu0902@gmail.com>
2026-07-10 18:06:36 +0530

    Intterrupt and yield

---
+++ src/com/threadsExamples/Interrupt.java
@@ -0,0 +1,26 @@
+package com.threadsExamples;
+
+public class Interrupt extends Thread{
+
+    public Interrupt(String name){
+        super(name);
+    }
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            System.out.println("tHREAD : "  + Thread.currentThread().getName() + " RUNNING");
+            Thread.yield();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Thread interrupted: " + e);
+        }
+    }
+    public static void main(String[] args) {
+        Interrupt t1 = new Interrupt("t1");
+        t1.start();
+
+        Interrupt t2 = new Interrupt("t2");
+        t2.start();
+        t1.interrupt();
+    }
+}
