b9564ad14485fee28911fef72859de5260782e5e
siddharthsahu0902 <siddharthsahu0902@gmail.com>
2026-07-16 13:23:47 +0530

    Fair v/s unfair lock

---
+++ src/com/threadsExamples/locks/FairLock.java
@@ -0,0 +1,39 @@
+package com.threadsExamples.locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairLock {
+    private final Lock fairLock = new ReentrantLock(true);
+
+    public void accessResouces(){
+        fairLock.lock();
+        try{
+            System.out.println("Accessing Resouces by accquring the lock by " + Thread.currentThread().getName());
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        finally{
+            System.out.println("Releasing the lock by " + Thread.currentThread().getName());
+            fairLock.unlock();
+        }
+    }
+    public static void main(String[] args) {
+        FairLock lock = new FairLock();
+        Runnable task  = new Runnable() {
+            @Override
+            public void run() {
+                lock.accessResouces();
+            }
+        };
+        Thread t1 = new Thread(task, "1");
+        Thread t2 = new Thread(task, "2");
+        Thread t3 = new Thread(task, "3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ src/com/threadsExamples/locks/UnfairLock.java
@@ -0,0 +1,39 @@
+package com.threadsExamples.locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class UnfairLock {
+    private final Lock unfairLock = new ReentrantLock();
+
+    public void accessResouces(){
+        unfairLock.lock();
+        try{
+            System.out.println("Accessing Resouces by accquring the lock by " + Thread.currentThread().getName());
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        finally{
+            System.out.println("Releasing the lock by " + Thread.currentThread().getName());
+            unfairLock.unlock();
+        }
+    }
+    public static void main(String[] args) {
+        UnfairLock lock = new UnfairLock();
+        Runnable task  = new Runnable() {
+            @Override
+            public void run() {
+                lock.accessResouces();
+            }
+        };
+        Thread t1 = new Thread(task, "1");
+        Thread t2 = new Thread(task, "2");
+        Thread t3 = new Thread(task, "3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
