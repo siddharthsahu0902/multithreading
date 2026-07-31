fc9f1cb1cd86d0a22068b33c4400cf2055bc3a69
siddharthsahu0902 <siddharthsahu0902@gmail.com>
2026-07-31 16:41:14 +0530

    Future and Callable example using ExecutorService submit()

---
+++ src/com/multihreadingExamples/executorFramework/FutureAndCallable.java
@@ -0,0 +1,33 @@
+package com.multihreadingExamples.executorFramework;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureAndCallable {
+    public static void main(String[] args) {
+         ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        Future<Integer> f1 = executor.submit(()->{
+            try{
+                Thread.sleep(3000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            return 1;
+        });
+
+        try{
+            System.out.println(f1.get());
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Executing the further code.");
+
+    }
+}
